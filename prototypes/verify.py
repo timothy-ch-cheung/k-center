30385022d773b099613e82a75ae7d779c6cf26ed
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-10-29 16:56:53 +0000

    Add function to verify whether a solution to colourful k-center is valid

--- prototypes/verify.py
+++ prototypes/verify.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from enum import Enum
+from typing import Dict, List
 import numpy
 
 
@@ -20,10 +21,52 @@ def create_graph():
         for m in G.nodes():
             if n == m:
                 continue
-            weight = numpy.linalg.norm(G.nodes[n]["pos"]-G.nodes[m]["pos"])
-            G.add_edge(n, m, weight=weight)
+            weight = numpy.linalg.norm(G.nodes[n]["pos"] - G.nodes[m]["pos"])
+            G.add_edge(n, m, key=str(n) + str(m), weight=weight)
     return G
 
 
-def verify_solution():
-    pass
+def verify_solution(graph: nx.Graph, constraints: Dict[Colour, int], k: int, radius: float, centers: List[int]) -> bool:
+    if len(centers) > k:
+        return False
+
+    clusters = {x:[] for x in centers}
+
+    # Calculate which center the node is clustered with
+    for node in graph.nodes():
+        nearest_center = None
+        min_dist = float("inf")
+        for center in centers:
+            if node not in graph[center]:
+                continue
+            edge = graph[center][node]
+            weight = edge["weight"] if edge is not None else float("inf")
+            if weight < min_dist:
+                min_dist = weight
+                nearest_center = center
+
+        if min_dist <= radius:
+            clusters[nearest_center].append(node)
+
+    coverage = {k: 0 for (k, v) in constraints.items()}
+
+    for (center, members) in clusters.items():
+        coverage[graph.nodes[center]["colour"]] += 1
+        for member in members:
+            coverage[graph.nodes[member]["colour"]] += 1
+
+    for colour in constraints.keys():
+        if coverage[colour] < constraints[colour]:
+            return False
+    return True
+
+
+def test(centers: List[int], radius: int):
+    constraints = {Colour.BLUE: 3, Colour.RED: 2}
+    k = 2
+    is_valid = verify_solution(create_graph(), constraints, k, radius, centers)
+    print("Solution with centers: ", centers, "valid:", is_valid)
+
+
+test([0, 4], 2)
+test([3, 4], 2)
