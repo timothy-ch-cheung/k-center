ca27e2bea6545cf9fad93ce898caf95687108825
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-10-31 17:59:54 +0000

    Add test case to verify the colourful k-center can be solved using greedy (for the basic graph)

--- tests/kcenter/solver/test_greedy.py
+++ tests/kcenter/solver/test_greedy.py
@@ -1,9 +1,12 @@
 import pytest
 
-from kcenter.constant.colour import Colour
+from kcenter.verify.verify import verify_solution, Colour
 from src.kcenter.solver.greedy import GreedySolver
 from tests.kcenter.util.create_test_graph import basic_graph
 
+
+CONSTRAINTS = {Colour.BLUE: 3, Colour.RED: 2}
+K = 2
 
 def test_max_dist_single_cluster():
     graph = basic_graph()
@@ -23,9 +26,17 @@ def test_max_dist_double_cluster():
 
 def test_greedy_basic_graph():
     graph = basic_graph()
-    instance = GreedySolver(graph, 2, {Colour.BLUE: 3, Colour.RED: 2})
+    instance = GreedySolver(graph, K, CONSTRAINTS)
     clusters, radius = instance.solve()
 
     assert radius == pytest.approx(0.854, 0.001)
     assert list(clusters.keys()) == [0, 4]
     assert list(clusters.values()) == [{0, 1, 2}, {3, 4}]
+
+
+def test_greedy_basic_graph_colourful_clustering():
+    graph = basic_graph()
+    instance = GreedySolver(graph, K, CONSTRAINTS)
+    clusters, radius = instance.solve()
+
+    assert verify_solution(graph, CONSTRAINTS, K, radius, set(clusters.keys())) is True
