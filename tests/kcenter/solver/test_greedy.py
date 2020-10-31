47721190ae94c2ca86164ccaae7f5c938e505568
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-10-31 18:37:21 +0000

    Add example to show greedy algorithm (Gonzalez) not giving optimal solution for relaxed constraints with outliers

--- tests/kcenter/solver/test_greedy.py
+++ tests/kcenter/solver/test_greedy.py
@@ -1,18 +1,20 @@
 import pytest
 
-from kcenter.verify.verify import verify_solution, Colour
+from src.kcenter.verify.verify import verify_solution, Colour
 from src.kcenter.solver.greedy import GreedySolver
-from tests.kcenter.util.create_test_graph import basic_graph
+from tests.kcenter.util.create_test_graph import basic_graph, basic_graph_with_outlier
 
-
-CONSTRAINTS = {Colour.BLUE: 3, Colour.RED: 2}
+FLOAT_ERROR_MARGIN = 0.001
+STRICT_CONSTRAINTS = {Colour.BLUE: 3, Colour.RED: 2}
+RELAXED_CONSTRAINTS = {Colour.BLUE: 2, Colour.RED: 2}
 K = 2
+
 
 def test_max_dist_single_cluster():
     graph = basic_graph()
     max_node, max_dist, owning_center = GreedySolver.max_dist(graph, clusters={0: {0, 1, 2, 3, 4}})
     assert max_node == 4
-    assert max_dist == pytest.approx(5.515, 0.001)
+    assert max_dist == pytest.approx(5.515, FLOAT_ERROR_MARGIN)
     assert owning_center == 0
 
 
@@ -20,23 +22,31 @@ def test_max_dist_double_cluster():
     graph = basic_graph()
     max_node, max_dist, owning_center = GreedySolver.max_dist(graph, clusters={0: {0, 1, 2}, 4: {3, 4}})
     assert max_node == 2
-    assert max_dist == pytest.approx(0.854, 0.001)
+    assert max_dist == pytest.approx(0.854, FLOAT_ERROR_MARGIN)
     assert owning_center == 0
-
-
-def test_greedy_basic_graph():
-    graph = basic_graph()
-    instance = GreedySolver(graph, K, CONSTRAINTS)
-    clusters, radius = instance.solve()
-
-    assert radius == pytest.approx(0.854, 0.001)
-    assert list(clusters.keys()) == [0, 4]
-    assert list(clusters.values()) == [{0, 1, 2}, {3, 4}]
 
 
 def test_greedy_basic_graph_colourful_clustering():
     graph = basic_graph()
-    instance = GreedySolver(graph, K, CONSTRAINTS)
+    instance = GreedySolver(graph, K, STRICT_CONSTRAINTS)
     clusters, radius = instance.solve()
 
-    assert verify_solution(graph, CONSTRAINTS, K, radius, set(clusters.keys())) is True
+    assert radius == pytest.approx(0.854, FLOAT_ERROR_MARGIN)
+    assert list(clusters.keys()) == [0, 4]
+    assert list(clusters.values()) == [{0, 1, 2}, {3, 4}]
+    assert verify_solution(graph, STRICT_CONSTRAINTS, K, radius, set(clusters.keys())) is True
+
+
+def test_greedy_basic_graph_outlier_colourful_clustering():
+    """This is an example of a colourful k-center graph that is not solved optimally with the standard greedy algorithm
+    by Gonzalez. The computed centers are still [0, 4], but node 2 becomes an outlier. The distance between node 2 and
+    the center 0 is 3.275. When we relax the constraints, we don't need to cover node 2, which means we cluster
+    with a much lower cost of 0.707"""
+    graph = basic_graph_with_outlier()
+    instance = GreedySolver(graph, K, RELAXED_CONSTRAINTS)
+    clusters, radius = instance.solve()
+
+    assert radius == pytest.approx(3.275, FLOAT_ERROR_MARGIN)
+    assert list(clusters.keys()) == [0, 4]
+    assert list(clusters.values()) == [{0, 1, 2}, {3, 4}]
+    assert verify_solution(graph, RELAXED_CONSTRAINTS, K, radius, set(clusters.keys())) is True
--- tests/kcenter/util/create_test_graph.py
+++ tests/kcenter/util/create_test_graph.py
@@ -4,20 +4,38 @@ import networkx as nx
 from src.kcenter.constant.colour import Colour
 
 
-def basic_graph() -> nx.Graph:
-        G = nx.Graph()
-        G.add_node(0, pos=numpy.array((1.3, 2.6)), colour=Colour.BLUE)
-        G.add_node(1, pos=numpy.array((1.2, 2.1)), colour=Colour.BLUE)
-        G.add_node(2, pos=numpy.array((0.5, 2.3)), colour=Colour.BLUE)
-        G.add_node(3, pos=numpy.array((5.9, 5.2)), colour=Colour.RED)
-        G.add_node(4, pos=numpy.array((6.4, 4.7)), colour=Colour.RED)
+def calculate_edges(graph: nx.Graph):
+    for n in graph.nodes():
+        for m in graph.nodes():
+            if n == m:
+                continue
+            weight = numpy.linalg.norm(graph.nodes[n]["pos"] - graph.nodes[m]["pos"])
+            graph.add_edge(n, m, key=str(n) + str(m), weight=weight)
 
-        # min weight = 0.5099
-        # max weight = 6.3695
-        for n in G.nodes():
-            for m in G.nodes():
-                if n == m:
-                    continue
-                weight = numpy.linalg.norm(G.nodes[n]["pos"] - G.nodes[m]["pos"])
-                G.add_edge(n, m, key=str(n) + str(m), weight=weight)
-        return G
+
+def basic_graph() -> nx.Graph:
+    G = nx.Graph()
+    G.add_node(0, pos=numpy.array((1.3, 2.6)), colour=Colour.BLUE)
+    G.add_node(1, pos=numpy.array((1.2, 2.1)), colour=Colour.BLUE)
+    G.add_node(2, pos=numpy.array((0.5, 2.3)), colour=Colour.BLUE)
+    G.add_node(3, pos=numpy.array((5.9, 5.2)), colour=Colour.RED)
+    G.add_node(4, pos=numpy.array((6.4, 4.7)), colour=Colour.RED)
+
+    # min weight = 0.5099
+    # max weight = 6.3695
+    calculate_edges(G)
+    return G
+
+
+def basic_graph_with_outlier() -> nx.Graph:
+    G = nx.Graph()
+    G.add_node(0, pos=numpy.array((1.3, 2.6)), colour=Colour.BLUE)
+    G.add_node(1, pos=numpy.array((1.2, 2.1)), colour=Colour.BLUE)
+    G.add_node(2, pos=numpy.array((-1.5, 4.3)), colour=Colour.RED)
+    G.add_node(3, pos=numpy.array((5.9, 5.2)), colour=Colour.RED)
+    G.add_node(4, pos=numpy.array((6.4, 4.7)), colour=Colour.RED)
+
+    # min weight = 0.5099
+    # max weight = 6.3695
+    calculate_edges(G)
+    return G
