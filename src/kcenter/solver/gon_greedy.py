8ef3189b591968991bac3d66b047319ecafc6fd5
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-10-31 02:29:43 +0000

    Modify gon algorithm to output clusters

--- src/kcenter/solver/gon_greedy.py
+++ src/kcenter/solver/gon_greedy.py
@@ -1,35 +1,68 @@
-from typing import Dict, Tuple, Set
+from typing import Dict, Tuple, Set, List
 
 import networkx as nx
 
 from src.kcenter.constant.colour import Colour
 from src.kcenter.solver.abstract_solver import AbstractSolver
 from src.kcenter.verify.verify import verify_solution
+from tests.kcenter.util.create_test_graph import basic_graph
 
 
 class GonGreedy(AbstractSolver):
     def __init__(self, graph: nx.Graph, k: int, constraints: Dict[Colour, int]):
         super().__init__(graph, k, constraints)
 
-    def solve(self) -> Tuple[Set[int], Set[Set[int]], int]:
-        centers = {0}
+    @staticmethod
+    def max_dist(graph: nx.Graph, centers: List[int], clusters: List[Set[int]]):
+        max_dist = -float("inf")
+        max_node = None
+        owning_center = None
+        for center, cluster in zip(centers, clusters):
+            for node in cluster:
+                if node == center:
+                    continue
+                dist = graph[center][node]["weight"]
+                if dist > max_dist:
+                    max_dist = dist
+                    max_node = node
+                    owning_center = center
+        return max_node, max_dist, owning_center
+
+    def solve(self) -> Tuple[List[int], List[Set[int]], int]:
+        centers = [0]
+        clusters = [set(list(self.graph.nodes))]
+
         for i in range(1, self.k):
-            max_dist = -float("inf")
-            max_node = None
-            for node in self.graph.nodes():
-                for center in centers:
-                    if node not in self.graph[center]:
-                        continue
-                    dist = self.graph[center][node]["weight"]
-                    if dist > max_dist:
-                        max_dist = dist
-                        max_node = node
-            centers.add(max_node)
+            max_node, max_dist, owning_center = GonGreedy.max_dist(self.graph, centers, clusters)
 
-        weights = nx.get_edge_attributes(self.graph, 'weight').values()
-        weights = list(set(weights))
-        weights = sorted(weights)
+            centers.append(max_node)
+            new_cluster = {max_node}
+            clusters[owning_center].remove(max_node)
 
-        for weight in weights:
-            if verify_solution(self.graph, self.constraints, self.k, weight, centers):
-                return centers, set(), weight
+            for center, cluster in zip(centers, clusters):
+                nodes_moved = []
+                for node in cluster:
+                    if node != max_node and node != center \
+                            and self.graph[node][max_node]["weight"] < self.graph[node][center]["weight"]:
+                        new_cluster.add(node)
+                        nodes_moved.append(node)
+
+                for node in nodes_moved:
+                    cluster.remove(node)
+
+            clusters.append(new_cluster)
+
+        radius = GonGreedy.max_dist(self.graph, centers, clusters)[1]
+        return centers, clusters, radius
+
+        # weights = nx.get_edge_attributes(self.graph, 'weight').values()
+        # weights = list(set(weights))
+        # weights = sorted(weights)
+        #
+        # for weight in weights:
+        #     if verify_solution(self.graph, self.constraints, self.k, weight, centers):
+        #         return centers, set(), weight
+
+
+instance = GonGreedy(basic_graph(), 2, {Colour.BLUE: 2, Colour.RED: 2})
+print(instance.solve())
---
+++ tests/kcenter/solver/test_gon_greedy.py
@@ -0,0 +1,11 @@
+import pytest
+
+from kcenter.solver.gon_greedy import GonGreedy
+from tests.kcenter.util.create_test_graph import basic_graph
+
+
+def test_max_dist():
+    graph = basic_graph()
+    max_node, max_dist = GonGreedy.max_dist(graph, centers=[0], clusters=[{0, 1, 2, 3, 4}])
+    assert max_node == 4
+    assert max_dist == pytest.approx(5.515, 0.001)
