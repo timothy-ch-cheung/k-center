5ed86a1280413d0ad942a153c88f7fc50fb72227
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-12-07 03:46:34 +0000

    Fix find_next test

--- tests/kcenter/solver/pbs/test_pbs_local_search.py
+++ tests/kcenter/solver/pbs/test_pbs_local_search.py
@@ -36,26 +36,26 @@ def test_find_next():
     graph = basic_graph()
     instance = PBS(graph, K, STRICT_CONSTRAINTS)
 
-    individual = Individual({0, 4, 3})
+    individual = Individual({0, 4})
     individual.init_nearest_centers(graph)
-    assert individual.cost == pytest.approx(5.515, FLOAT_ERROR)
-    expected_nearest_centers = [
-        "{'nearest_center': {point: 0, cost: 0}, 'second_nearest_center': None}",
-        "{'nearest_center': {point: 0, cost: 0.51}, 'second_nearest_center': None}",
-        "{'nearest_center': {point: 0, cost: 0.854}, 'second_nearest_center': None}",
-        "{'nearest_center': {point: 0, cost: 5.284}, 'second_nearest_center': None}",
-        "{'nearest_center': {point: 0, cost: 5.515}, 'second_nearest_center': None}"
-    ]
-    for point, expected in enumerate(expected_nearest_centers):
-        assert str(individual.nearest_centers[point]) == expected
-
-    instance.add_center(center=4, individual=individual)
+    assert individual.cost == pytest.approx(0.854, FLOAT_ERROR)
     expected_nearest_centers = [
         "{'nearest_center': {point: 0, cost: 0}, 'second_nearest_center': {point: 4, cost: 5.515}}",
         "{'nearest_center': {point: 0, cost: 0.51}, 'second_nearest_center': {point: 4, cost: 5.814}}",
         "{'nearest_center': {point: 0, cost: 0.854}, 'second_nearest_center': {point: 4, cost: 6.369}}",
         "{'nearest_center': {point: 4, cost: 0.707}, 'second_nearest_center': {point: 0, cost: 5.284}}",
         "{'nearest_center': {point: 4, cost: 0}, 'second_nearest_center': {point: 0, cost: 5.515}}"
+    ]
+    for point, expected in enumerate(expected_nearest_centers):
+        assert str(individual.nearest_centers[point]) == expected
+
+    instance.add_center(center=3, individual=individual)
+    expected_nearest_centers = [
+        "{'nearest_center': {point: 0, cost: 0}, 'second_nearest_center': {point: 3, cost: 5.284}}",
+        "{'nearest_center': {point: 0, cost: 0.51}, 'second_nearest_center': {point: 3, cost: 5.63}}",
+        "{'nearest_center': {point: 0, cost: 0.854}, 'second_nearest_center': {point: 3, cost: 6.129}}",
+        "{'nearest_center': {point: 3, cost: 0}, 'second_nearest_center': {point: 4, cost: 0.707}}",
+        "{'nearest_center': {point: 4, cost: 0}, 'second_nearest_center': {point: 3, cost: 0.707}}"
     ]
     for point, expected in enumerate(expected_nearest_centers):
         assert str(individual.nearest_centers[point]) == expected
@@ -76,7 +76,6 @@ def test_remove_center():
         "{'nearest_center': {point: 4, cost: 0}, 'second_nearest_center': {point: 0, cost: 5.515}}"
     ]
 
-    instance.remove_center(center=4, individual=individual)
     for point, expected in enumerate(expected_nearest_centers):
         assert str(individual.nearest_centers[point]) == expected
     expected_nearest_centers = [
@@ -86,5 +85,6 @@ def test_remove_center():
         "{'nearest_center': {point: 0, cost: 5.284}, 'second_nearest_center': None}",
         "{'nearest_center': {point: 0, cost: 5.515}, 'second_nearest_center': None}"
     ]
+    instance.remove_center(center=4, individual=individual)
     for point, expected in enumerate(expected_nearest_centers):
         assert str(individual.nearest_centers[point]) == expected
