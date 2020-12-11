60c2e3526aca73e6d6867d46c6aa16b885ed0640
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-12-11 23:58:09 +0000

    Fix import

--- src/kcenter/pbs/pbs.py
+++ src/kcenter/pbs/pbs.py
@@ -1,5 +1,4 @@
 import random
-from bisect import bisect_left
 from typing import Tuple, Dict, Set, Generator, List
 
 import networkx as nx
--- src/server/routes.py
+++ src/server/routes.py
@@ -2,7 +2,7 @@ import time
 
 from flask import render_template, request, jsonify, Blueprint
 
-from kcenter.pbs.pbs import PBS
+from src.kcenter.pbs.pbs import PBS
 from src.kcenter.bandyapadhyay.solver import ConstantColourfulKCenterSolver
 from src.kcenter.constant.colour import Colour
 from src.kcenter.greedy.greedy import GreedySolver
