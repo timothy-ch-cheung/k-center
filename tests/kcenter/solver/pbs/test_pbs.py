3f6a6c77a5d603d3d24ba8ed3e5e88ed9e0c58f8
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-12-11 20:29:53 +0000

    Add integration test case for pbs outlier graph

--- tests/kcenter/solver/pbs/test_pbs.py
+++ tests/kcenter/solver/pbs/test_pbs.py
@@ -1,9 +1,10 @@
 import pytest
 
+from src.kcenter.verify.verify import verify_solution
 from src.kcenter.constant.colour import Colour
 from src.kcenter.pbs.pbs import PBS
 from tests.kcenter.constant.consts import FLOAT_ERROR
-from tests.kcenter.util.create_test_graph import basic_graph
+from tests.kcenter.util.create_test_graph import basic_graph, basic_graph_with_outlier
 
 FLOAT_ERROR_MARGIN = 0.001
 STRICT_CONSTRAINTS = {Colour.BLUE: 3, Colour.RED: 2}
@@ -22,7 +23,7 @@ def test_get_nwk():
     assert nwk == [0, 1, 2]
 
 
-def test_pbs(seed_random):
+def test_pbs_basic_graph(seed_random):
     graph = basic_graph()
     instance = PBS(graph, K, STRICT_CONSTRAINTS)
     clusters, outliers, radius = instance.solve()
@@ -30,3 +31,15 @@ def test_pbs(seed_random):
     assert clusters == {1: {0, 1, 2}, 3: {3, 4}}
     assert outliers == set()
     assert radius == pytest.approx(0.728, FLOAT_ERROR)
+    assert verify_solution(graph, STRICT_CONSTRAINTS, K, radius, set(clusters.keys())) is True
+
+
+def test_pbs_basic_graph_with_outlier(seed_random):
+    graph = basic_graph_with_outlier()
+    instance = PBS(graph, K, STRICT_CONSTRAINTS)
+    clusters, outliers, radius = instance.solve()
+
+    assert clusters == {0: {0, 1, 2}, 3: {3, 4}}
+    assert outliers == set()
+    assert radius == pytest.approx(3.785, FLOAT_ERROR)
+    assert verify_solution(graph, STRICT_CONSTRAINTS, K, radius, set(clusters.keys())) is True
