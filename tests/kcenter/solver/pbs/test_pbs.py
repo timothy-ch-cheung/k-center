c0245ec23809bb1e0a455d4ec01d2d5f58c25efd
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-12-13 12:43:09 +0000

    Add integration test for larger graph

--- tests/kcenter/solver/pbs/test_pbs.py
+++ tests/kcenter/solver/pbs/test_pbs.py
@@ -4,7 +4,7 @@ from src.kcenter.verify.verify import verify_solution
 from src.kcenter.constant.colour import Colour
 from src.kcenter.pbs.pbs import PBS
 from tests.kcenter.constant.consts import FLOAT_ERROR
-from tests.kcenter.util.create_test_graph import basic_graph, basic_graph_with_outlier
+from tests.kcenter.util.create_test_graph import basic_graph, basic_graph_with_outlier, medium_graph
 
 FLOAT_ERROR_MARGIN = 0.001
 STRICT_CONSTRAINTS = {Colour.BLUE: 3, Colour.RED: 2}
@@ -43,3 +43,14 @@ def test_pbs_basic_graph_with_outlier(seed_random):
     assert outliers == set()
     assert radius == pytest.approx(3.785, FLOAT_ERROR)
     assert verify_solution(graph, STRICT_CONSTRAINTS, K, radius, set(clusters.keys())) is True
+
+
+def test_pbs_medium_graph(seed_random):
+    graph = medium_graph()
+    instance = PBS(graph, 3, STRICT_CONSTRAINTS)
+    clusters, outliers, radius = instance.solve()
+
+    assert clusters == {0: {0, 1}, 4: {3, 4}, 6: {2, 5, 6, 7, 8, 9}}
+    assert outliers == set()
+    assert radius == pytest.approx(1.063, FLOAT_ERROR)
+    assert verify_solution(graph, STRICT_CONSTRAINTS, 3, radius, set(clusters.keys())) is True
