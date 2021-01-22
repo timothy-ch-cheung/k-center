1c6320486371ab5ff2f19c9acd96f4ddf4567789
Tim Cheung <timothy.ch.cheung@gmail.com>
2021-01-22 16:28:36 +0000

    Add type hints to similarity code

--- src/kcenter/pbs/similarity.py
+++ src/kcenter/pbs/similarity.py
@@ -1,5 +1,5 @@
 import math
-from typing import Tuple
+from typing import Tuple, Set
 
 import numpy as np
 import networkx as nx
@@ -13,21 +13,21 @@ class CompareSolution:
         self.min_value = min_value
         self.max_value = max_value
 
-    def normalise(self, S):
-        def tup_diff(a, b):
+    def normalise(self, S: Set[Tuple[float, float]]):
+        def tup_diff(a: Tuple[float, float], b: Tuple[float, float]):
             return a[0] - b[0], a[1] - b[1]
 
-        def tup_div(a, b):
+        def tup_div(a: Tuple[float, float], b: Tuple[float, float]):
             return a[0] / (b[0] if b[0] != 0 else 1), a[1] / (b[1] if b[1] != 0 else 1)
 
-        def apply_normalise(s):
+        def apply_normalise(s: Set[Tuple[float, float]]):
             return {tup_div(tup_diff(self.graph.nodes()[x]["pos"], self.min_value),
                             tup_diff(self.max_value, self.min_value)) for x in s}
 
         S = apply_normalise(S)
         return S
 
-    def sim(self, A, B):
+    def sim(self, A: Set[Tuple[float, float]], B: Set[Tuple[float, float]]):
         A = self.normalise(A)
         B = self.normalise(B)
 
