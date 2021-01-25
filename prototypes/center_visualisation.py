17d93db8b07e9a77a0534ed9111f9d6dd88700cb
Tim Cheung <timothy.ch.cheung@gmail.com>
2021-01-25 00:39:09 +0000

    print similarity score and make points transparent

--- prototypes/center_visualisation.py
+++ prototypes/center_visualisation.py
@@ -1,9 +1,10 @@
 import matplotlib as matplotlib
 import matplotlib.pyplot as plt
 
+from kcenter.pbs.similarity import CompareSolution
 from server.graph_loader import GraphLoader
 
-colors = ["black", "yellow", "red"]
+colors = ["#00000040", "#00FF0080", "#FF000080"]
 
 graph = GraphLoader.get_graph("medium")
 x = [graph.nodes()[i]["pos"][0] for i in graph.nodes()]
@@ -21,12 +22,12 @@ expected_population = [
     {0, 18, 3, 13}
 ]
 
-first_individual = 0
+first_individual = 3
 x += [graph.nodes()[i]["pos"][0] for i in expected_population[first_individual]]
 y += [graph.nodes()[i]["pos"][1] for i in expected_population[first_individual]]
 color_indices += [1 for x in range(4)]
 
-second_individual = 1
+second_individual = 7
 x += [graph.nodes()[i]["pos"][0] for i in expected_population[second_individual]]
 y += [graph.nodes()[i]["pos"][1] for i in expected_population[second_individual]]
 color_indices += [2 for x in range(4)]
@@ -35,3 +36,12 @@ color_indices += [2 for x in range(4)]
 colormap = matplotlib.colors.ListedColormap(colors)
 plt.scatter(x, y, c=color_indices, cmap=colormap)
 plt.show()
+
+# Similarity
+
+MIN = (34.78753471028318, 42.83698613479884)
+MAX = (196.61095005621613, 178.77089875665393)
+comp = CompareSolution(graph, min_value=MIN, max_value=MAX)
+
+print(comp.sim(expected_population[first_individual], expected_population[second_individual]))
+print(comp.sim(expected_population[second_individual], expected_population[first_individual]))
