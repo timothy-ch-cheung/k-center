e783ff2cc52b62feae04f9b4cc6b5a9cb4feec3b
Tim Cheung <timothy.ch.cheung@gmail.com>
2021-01-28 03:20:50 +0000

    Fix find cost tests in colourful pbs

--- tests/kcenter/solver/colourful_pbs/test_find_cost.py
+++ tests/kcenter/solver/colourful_pbs/test_find_cost.py
@@ -40,31 +40,24 @@ def test_find_cost_basic_graph_outlier_non_optimal(seed_random):
 
 
 def test_find_cost_large_graph_bad_centers(seed_random):
-    constraints = {Colour.BLUE: 2, Colour.RED: 2}
-    k = 2
-    graph = GraphLoader.get_graph("basic_with_outlier")
+    constraints = {Colour.BLUE: 50, Colour.RED: 50}
+    k = 5
+    graph = GraphLoader.get_graph("large")
     instance = ColourfulPBS(graph, k, constraints)
 
-    nearest_centers = {
-        0: NearestCenters(Neighbour(3, 5.2839), Neighbour(4, 5.5154)),
-        1: NearestCenters(Neighbour(3, 5.6303), Neighbour(4, 5.8138)),
-        2: NearestCenters(Neighbour(3, 5.5109), Neighbour(4, 6.1131)),
-        3: NearestCenters(Neighbour(3, 0.0), Neighbour(4, 0.7071)),
-        4: NearestCenters(Neighbour(4, 0.0), Neighbour(3, 0.7071))
-    }
-    optimised_individual = Individual({3, 4}, 5.6303, nearest_centers)
+    optimised_individual = Individual({110, 111, 112, 113, 114})
 
     optimised_individual = instance.find_cost(optimised_individual)
-    assert optimised_individual.cost == pytest.approx(5.511, FLOAT_ERROR_MARGIN)
+    assert optimised_individual.cost == pytest.approx(199.4437, FLOAT_ERROR_MARGIN)
 
 
 def test_find_cost_large_graph(seed_random):
     constraints = {Colour.BLUE: 50, Colour.RED: 50}
-    k = 2
+    k = 5
     graph = GraphLoader.get_graph("large")
     instance = ColourfulPBS(graph, k, constraints)
 
-    optimised_individual = Individual({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 46.0624)
+    optimised_individual = Individual({0, 1, 2, 3, 4}, 46.0624)
     optimised_individual.init_nearest_centers(instance.points, instance.weights)
 
     optimised_individual = instance.find_cost(optimised_individual)
