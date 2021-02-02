212b42a303f8fb970500d8446070a64a52cb1b46
Tim Cheung <timothy.ch.cheung@gmail.com>
2021-02-02 00:56:11 +0000

    Add algorithm type

--- webapp/src/constants/algorithms.ts
+++ webapp/src/constants/algorithms.ts
@@ -1,31 +1,38 @@
 interface algorithm_props {
     name: String
     short_name: String
+    type: String
 }
 
 export const algorithms : {[key: string]: algorithm_props}= {
     greedy: {
         name: "Greedy (González 1985)",
-        short_name: "Greedy"
+        short_name: "Greedy",
+        type: "approximation"
     },
     greedy_reduce: {
         name: "Greedy (modified to optimise radii)",
-        short_name: "Greedy Reduce"
+        short_name: "Greedy Reduce",
+        type: "approximation"
     },
     colourful_bandyapadhyay_pseudo: {
         name: "O(1)-colourful (Pseudo 2-approximation, Bandyapadhyay et al. 2019)",
-        short_name: "Pseudo O(1)-colourful"
+        short_name: "Pseudo O(1)-colourful",
+        type: "approximation"
     },
     colourful_bandyapadhyay: {
         name: "O(1)-colourful (17-approximation, Bandyapadhyay et al. 2019)",
-        short_name: "O(1)-colourful"
+        short_name: "O(1)-colourful",
+        type: "approximation"
     },
     pbs: {
         name: "PBS (Pullan 2008)",
-        short_name: "PBS"
+        short_name: "PBS",
+        type: "genetic"
     },
     colourful_pbs: {
         name: "Colourful PBS",
-        short_name: "Colourful PBS"
+        short_name: "Colourful PBS",
+        type: "genetic"
     }
 }
