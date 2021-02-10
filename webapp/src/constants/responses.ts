e2a19c98d4a2b66ee20f26a559f49df867ba9ded
Tim Cheung <timothy.ch.cheung@gmail.com>
2021-02-10 21:40:56 +0000

    Correct spelling

--- webapp/src/constants/responses.ts
+++ webapp/src/constants/responses.ts
@@ -11,14 +11,14 @@ export const responses: { [key: string]: algorithm_props } = {
     brute_force_k_center_predicted_time: {
         message: "how was this calculated?",
         additionalInfo: `The number of candidates, from the set of points length n, with k centers is n choose k (ⁿCₖ). 
-        A small number of candidates are timed an checked and an average is taken to find the time to check a typical 
-        candidate (Τ). Therefore the worst case estimated time is ⁿCₖ × Τ.`
+        The time to check a small number of candidates is recorded and an average is taken to find the time to check a 
+        typical candidate (Τ). Therefore the worst case estimated time is ⁿCₖ × Τ.`
     },
     brute_force_colourful_k_center_predicted_time: {
         message: "how was this calculated?",
         additionalInfo: `The number of candidates, from the set of points length n, with k centers is n choose k (ⁿCₖ). 
-        There are also n² different weights that need to be tested for each candidate. A small number of candidates are 
-        timed an checked and an average is taken to find the time to check a typical candidate (Τ). Therefore the worst 
-        case the estimated time is n² × ⁿCₖ × Τ.`
+        There are also n² different weights that need to be tested for each candidate. The time to check a small number 
+        of candidates is recorded and an average is taken to find the time to check a typical candidate (Τ). Therefore 
+        the worst case the estimated time is n² × ⁿCₖ × Τ.`
     }
 }
