459cc6ea676a3174f62d1073d3eb98ea69dc1159
Tim Cheung <timothy.ch.cheung@gmail.com>
2021-02-10 00:40:06 +0000

    Install glpk on docker container and fix docker deploys for staging environment

--- webapp/src/API.ts
+++ webapp/src/API.ts
@@ -4,7 +4,7 @@ let apiURL;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     apiURL = "http://localhost:5000/api/v1"
 } else if (/((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}/.test(window.location.hostname)) {
-    apiURL = `http://${window.location.hostname}:5000/api/v1`
+    apiURL = `${window.location.origin}/api/v1`
 } else {
     apiURL = `https://${window.location.hostname}/api/v1`
 }
