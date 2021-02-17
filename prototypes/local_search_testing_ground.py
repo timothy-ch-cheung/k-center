2343ee4239c9f1e4291589f0daba938907545ada
Tim Cheung <timothy.ch.cheung@gmail.com>
2021-02-17 14:55:15 +0000

    Add code to observe local search behaviour

--- prototypes/local_search_testing_ground.py
+++ prototypes/local_search_testing_ground.py
@@ -3,23 +3,23 @@ import random
 from kcenter.constant.colour import Colour
 from kcenter.pbs.pbs import PBS, Individual
 from server.graph_loader import GraphLoader
+from server.orlib_graph_loader import ORLIBGraphLoader
 
 random.seed(0)
 
-graph = GraphLoader.get_graph("large")
-constraints = {Colour.BLUE: 50, Colour.RED: 50}
-k = 5
+graph = ORLIBGraphLoader.get_graph("pmed3")
+constraints = {}
+k = 10
 
 instance = PBS(graph, k, constraints)
 
-sol1 = Individual({106, 12, 81, 89, 92})
+sol1 = Individual({32, 36, 77, 48, 81, 18, 82, 49, 52, 87})
 instance.init_individual(sol1)
 
-sol2 = Individual({106, 12, 45, 86, 92})
+sol2 = Individual({32, 36, 77, 48, 17, 82, 51, 52, 55, 87})
 instance.init_individual(sol2)
 
-opt1 = instance.local_search(sol1, 2)
-
-opt2 = instance.local_search(sol2, 2)
+for i in range(20):
+    sol1 = instance.local_search(instance.mutation_random(sol1), 20)
 
 print()
