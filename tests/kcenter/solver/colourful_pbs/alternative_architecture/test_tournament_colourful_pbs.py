4f4ec8647985e7b252336519bc8691d9baf91fd8
Timothy Cheung <timothycheung@pop-os>
2021-04-08 21:50:57 +0100

    Add tournament selection

---
+++ tests/kcenter/solver/colourful_pbs/alternative_architecture/test_tournament_colourful_pbs.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.kcenter.colourful_pbs.alternative_architecture.tournament_colourful_pbs import TournamentColourfulPBS
+from src.kcenter.constant.colour import Colour
+from src.kcenter.pbs.pbs import Individual
+from tests.kcenter.util.create_test_graph import basic_graph_with_outlier
+from tests.kcenter.util.genetic import generate_test_population_basic_graph
+
+
+@pytest.fixture
+def instance():
+    constraints = {Colour.BLUE: 2, Colour.RED: 2}
+    k = 2
+    graph = basic_graph_with_outlier()
+    instance = TournamentColourfulPBS(graph, k, constraints)
+    instance.population = generate_test_population_basic_graph()
+    return instance
+
+
+def test_roulette_selection(seed_random, instance):
+    mating_pool = instance.selection()
+    assert len(mating_pool) == 4
+    assert mating_pool == [Individual({3, 4}), Individual({0, 1}), Individual({1, 2}), Individual({2, 3})]
