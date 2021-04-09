7648cace4e71c755873ef4e0dacb1eb647f5289a
Tim Cheung <timothy.ch.cheung@gmail.com>
2021-04-09 12:50:18 +0100

    Add partial pbs results

--- src/server/routes.py
+++ src/server/routes.py
@@ -3,6 +3,7 @@ import time
 import networkx as nx
 from flask import render_template, request, jsonify, Blueprint
 
+from kcenter.pbs.target_pbs import TargetPBS
 from src.kcenter.plateau_surfer.plateau_surfer import PlateauSurfer
 from src.kcenter.bandyapadhyay.pseudo_solver import ConstantPseudoColourful
 from src.kcenter.bandyapadhyay.solver import ConstantColourful
@@ -23,6 +24,7 @@ k_center_algorithms = {
     "colourful_bandyapadhyay_pseudo": ConstantPseudoColourful,
     "colourful_bandyapadhyay": ConstantColourful,
     "pbs": PBS,
+    "target_pbs": TargetPBS,
     "colourful_pbs": ColourfulPBS,
     "brute_force_k_center": BruteForceKCenter,
     "brute_force_colourful_k_center": BruteForceColourfulKCenter,
