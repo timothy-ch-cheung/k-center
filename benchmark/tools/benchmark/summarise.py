ff694faafdc3e3aae635ca02242a7f696b091d44
Tim Cheung <timothy.ch.cheung@gmail.com>
2021-04-16 01:47:35 +0100

    Update script to produce table with %-gap column

--- benchmark/tools/benchmark/summarise.py
+++ benchmark/tools/benchmark/summarise.py
@@ -17,29 +17,34 @@ results_type = Dict[str, float]
 instance_results_type = Dict[str, results_type]
 summary_type = Dict[str, Dict[str, instance_results_type]]
 
+
 def generate_latex_table(summaries: summary_type, dataset: List[str], type: str):
     print()
     DECIMAL_POINTS = 2
     algs = list(summaries.keys())
 
-    header = "\\multicolumn{5}{c}{Instance}"
+    header = "\\multicolumn{1}{c}{Instance}"
     for algorithm in algs:
         header += "& \\quad & \\multicolumn{4}{c}{" + algorithm + "}"
+    header += "& \\quad & multicolumn{2}{c}{Statistics}"
     print(f"{header}\\\\")
 
-    sub_underline = "\\cline{1-5}"
+    sub_underline = "\\cline{1-1}"
     COLUMN_WIDTH = 4
-    for idx,algorithm in enumerate(algs):
+    for idx, algorithm in enumerate(algs):
         sub_underline += " \\cline{" + str(idx + 2 + COLUMN_WIDTH * idx) + "-" + str(idx + 4 + COLUMN_WIDTH * idx) + "}"
 
     print(f"{sub_underline}\\\\")
 
-    sub_header = "Name & n & k & b & r"
+    sub_header = "name"
     for algorithm in algs:
         sub_header += f" && min & $\\mu$ & $\\sigma$ & mean time"
+    sub_header += " && \%-gap (cost) & \%-gap (time)"
 
     print(f"{sub_header}\\\\")
 
+    time_gaps = []
+    cost_gaps = []
     for problem in problem_list[dataset]:
         graph = loader[dataset].get_graph(problem)
         n = graph.graph["n"]
@@ -47,7 +52,7 @@ def generate_latex_table(summaries: summary_type, dataset: List[str], type: str)
         min_blue = graph.graph["min_blue"]
         min_red = graph.graph["min_red"]
 
-        row = f"{problem} & {n} & {k} & {min_blue} & {min_red}"
+        row = f"{problem}"
         for algorithm in algs:
             minimum = '{:.2f}'.format(round(summaries[algorithm][problem][type]["min"], DECIMAL_POINTS))
             mean = '{:.2f}'.format(round(summaries[algorithm][problem][type]["mean"], DECIMAL_POINTS))
@@ -55,7 +60,21 @@ def generate_latex_table(summaries: summary_type, dataset: List[str], type: str)
             mean_time = '{:.2f}'.format(round(summaries[algorithm][problem]["cost"]["mean"], DECIMAL_POINTS))
             row += f" && {minimum} & {mean} & {std} & {mean_time}"
 
+        cost_gap = ((summaries[algs[1]][problem]["cost"]["mean"] / summaries[algs[0]][problem]["cost"][
+            "mean"]) - 1) * 100
+        cost_gap_rounded = '{:.2f}'.format(round(cost_gap, DECIMAL_POINTS))
+        time_gap = ((summaries[algs[1]][problem]["time"]["mean"] / summaries[algs[0]][problem]["time"][
+            "mean"]) - 1) * 100
+        time_gap_rounded = '{:.2f}'.format(round(time_gap, DECIMAL_POINTS))
+        row += f" && {cost_gap_rounded} & {time_gap_rounded}"
+
+        time_gaps.append(time_gap)
+        cost_gaps.append(cost_gap)
+
         print(f"{row}\\\\")
+
+    footer = "\\multicolumn{10}{c}{} & Average && " + f"{np.mean(cost_gaps)} & {np.mean(time_gaps)}"
+    print(f"{footer}\\\\")
     print()
 
 
