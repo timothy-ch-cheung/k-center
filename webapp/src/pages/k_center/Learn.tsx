21013f1cbec39580876c8757d9f38dfca02f275d
Tim Cheung <timothy.ch.cheung@gmail.com>
2021-04-17 23:50:58 +0100

    Fix typos

--- webapp/src/pages/k_center/Learn.tsx
+++ webapp/src/pages/k_center/Learn.tsx
@@ -11,8 +11,9 @@ import rk_center_initial from "../../assets/2a_robust_k_center_initial.png"
 import rk_center_task from "../../assets/2b_robust_k_center_task.png"
 import rk_center_solution from "../../assets/2c_robust_k_center_solution.png"
 import ck_center_label from "../../assets/3a_colourful_points.png"
-import ck_center_task from "../../assets/3b_colourful_task.png"
-import ck_center_solution from "../../assets/3c_colourful_solution.png"
+import rkck_solution from "../../assets/3b_colourful_robust_solution.png"
+import ck_center_task from "../../assets/3c_colourful_task.png"
+import ck_center_solution from "../../assets/3d_colourful_solution.png"
 import styled from '@emotion/styled'
 
 interface Slide {
@@ -52,18 +53,18 @@ function Learn() {
 
     const slides: Array<Slide> = [
         {
-            img: k_center_initial, title: "What is the k-center problem?", description: `Imagine you were planning a city 
-        with a parcel delivery network and the n points represent potential locations for residents or warehouses. 
+            img: k_center_initial, title: "What is the k-center problem?", description: `Imagine you are planning a city 
+        with a parcel delivery network and the points represent potential locations for residents or warehouses. 
         The parcels will be delivered from the warehouse closest to the resident.`
         },
         {
-            img: k_center_initial, title: "What is the k-center problem?", description: `What is the best locations to build 
-        them such the the distance that a delivery driver must travel is minimised? Alternatively where should 
-        warehouses be built such that the maximum distance between a resident and their nearest warehouse is 
-        minimised?`
+            img: k_center_initial, title: "What are the k-center problem?", description: `What is the best locations to build 
+        warehouses such that the distance a delivery driver must travel is minimised? Another way to state this is 
+        where should  warehouses be built such that the maximum distance between a resident and their nearest warehouse 
+        is minimised?`
         },
         {
-            img: k_center_task, title: "What is the k-center problem?", description: `What if the budget was limited
+            img: k_center_task, title: "The constraint k", description: `What if the budget was limited
         and you only have enough money to build k warehouses? For example, what if your budget limited you to building 
         two warehouses, where should they be located?`
         },
@@ -81,7 +82,7 @@ function Learn() {
         {
             img: rk_center_task, title: "An intermediate problem", description: `Therefore we might only want to 
         consider a certain percentage of residents when considering warehouse locations to serve the majority rather
-        than the few. In our example lets say we want to consider at least 75% of residents.`
+        than the few. In our example lets say we want to consider at least 62.5% of residents.`
         },
         {
             img: rk_center_solution, title: "The Robust k-center problem", description: `This is called the Robust 
@@ -95,9 +96,14 @@ function Learn() {
             like to have the ambulances arrive to houses as fast as possible.`
         },
         {
-            img: ck_center_label, title: "Taking prior knowledge into account", description: `Assume we know that 
-            people aged over 60 tend to live in the countryside and they are more likely to require an ambulance. The
-            robust k-center problem would exclude them even though they need the service more.`
+            img: ck_center_label, title: "Demographics of the residents", description: `Assume we have some 
+            knowledge about the residents. For example, people aged over 60 tend to live in the countryside and they 
+            are more likely to require an ambulance due to higher likelihood of health complications. We can label
+            the two groups as blue and red.`
+        },
+        {
+            img: rkck_solution, title: "The robust k-center bias", description: `The solution provided by the
+            robust k-center problem would exclude people over 60 even though they need the service more.`
         },
         {
             img: ck_center_task, title: "Taking prior knowledge into account", description: `We can label residents
