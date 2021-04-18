e80be7e348c724ffff83cb60d2accf823e73b686
Tim Cheung <timothy.ch.cheung@gmail.com>
2021-04-18 13:08:25 +0100

    Add test for stepped solve

--- integration_tests/cypress/integration/examples/step.spec.js
+++ integration_tests/cypress/integration/examples/step.spec.js
@@ -14,4 +14,30 @@ context('Step', () => {
         cy.compareSnapshot("steps_modal", TOLERANCE)
     })
 
+    it('Visual regression on stepped walk through for greedy algorithm', () => {
+        cy.get("[data-cy=steps-modal-btn]").click()
+        cy.get('[cy-data=problem-instance-select]').click()
+        cy.get('[cy-data=basic-instance]').click()
+        cy.get('[cy-data=algorithm-select]').click()
+        cy.get('[cy-data="greedy').click()
+
+        cy.get('[cy-data=solve-submit-btn]').click()
+        cy.compareSnapshot("steps_walkthrough_00", TOLERANCE)
+        cy.get('[cy-data=page-prev]').should('be.disabled')
+
+        const STEPS = 3
+        for(let i = 1; i < STEPS + 1; i++) {
+            cy.get('[cy-data=page-next]').click()
+            cy.compareSnapshot(`steps_walkthrough_${i.toString().padStart(2, "0")}`, TOLERANCE)
+        }
+        cy.get('[cy-data=page-next]').should('be.disabled')
+        cy.get('[cy-data=page-prev]').should('not.be.disabled')
+
+        for(let i = 0; i < STEPS -1; i++) {
+            cy.get('[cy-data=page-prev]').click()
+        }
+        cy.get('[cy-data=page-prev]').should('be.disabled')
+        cy.get('[cy-data=page-next]').should('not.be.disabled')
+    })
+
 })
